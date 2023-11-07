6ecf20319c616e07251781ddfd0c75e4fb271adb
Kuba <kubamalewicz002@gmail.com>
2023-11-07 22:35:29 +0100

    Add logging out

--- Skydiving-Fronend/src/app/topbar/topbar.component.ts
+++ Skydiving-Fronend/src/app/topbar/topbar.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Router } from '@angular/router';
 import { TranslocoService } from '@ngneat/transloco';
 
 @Component({
@@ -9,11 +10,16 @@ import { TranslocoService } from '@ngneat/transloco';
 export class TopbarComponent {
   currentLanguage!: string
 
-  constructor(private translocoService: TranslocoService) {
+  constructor(private translocoService: TranslocoService, private router: Router) {
     this.currentLanguage = this.translocoService.getActiveLang();
   }
  
   public changeLanguage(languageCode: any): void {
     this.translocoService.setActiveLang(languageCode.target.value);
+  }
+
+  logout(){
+    window.localStorage.clear();
+    this.router.navigate(['']);
   }
 }
